9d7d842819e302a731020a01180f725b76858d63
jahona <jahona5240@gmail.com>
2018-08-13 17:09:35 +0900

    create-create-app, create Movie Component

---
+++ src/Movie.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Movie.css';
+
+class Movie extends Component {
+    render() {
+        return <h1>Hello this is a Movie</h1>
+    }
+}
+
+export default Movie;
