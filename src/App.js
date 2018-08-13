a20aab9370c8fd4d04d6600b49fc8ef90ed08854
jahona <jahona5240@gmail.com>
2018-08-14 00:11:55 +0900

    send data in props from parent node to child node

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import Movie from './Movie';
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
+
+const movieTitles = ["Movie1", "Movie2", "Movie3"];
+
+const movieImages = ["http://ww2.sjkoreancatholic.org/files/testing_image.jpg"];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie />
+        <Movie title={movieTitles[0]} poster={movieImages[0]} />
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -3,7 +3,20 @@ import './Movie.css';
 
 class Movie extends Component {
     render() {
-        return <h1>Hello this is a Movie</h1>
+        return (
+            <div>
+                <MoviePoster poster={this.props.poster}/>
+                <h1>{this.props.title}</h1>
+            </div>
+        )
+    }
+}
+
+class MoviePoster extends Component {
+    render() {
+        return(
+            <img src={this.props.poster}/>
+        )
     }
 }
 
