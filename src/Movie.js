4a3d6fcaab14f40bbd8a8d6d2b0c9149bcde569f
jahona <jahona5240@gmail.com>
2018-08-14 03:17:24 +0900

    class component(smart), functional component(dumb)

--- src/Movie.js
+++ src/Movie.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
+/*
+// class Component
 class Movie extends Component {
     static propTypes = {
         title: PropTypes.string,
@@ -17,13 +19,47 @@ class Movie extends Component {
         )
     }
 }
+*/
 
+// functional Component
+function Movie({title, poster}) {
+    return (
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+/*
+// class Component
 class MoviePoster extends Component {
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
+
     render() {
         return(
-            <img src={this.props.poster}/>
+            <img src={this.props.poster} alt="Movie Poster"/>
         )
     }
+}
+*/
+
+// functional Component
+function MoviePoster({poster}) {
+    return (
+        <img src={poster} alt="Movie Poster"/>
+    )
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
